c1d26647f2f3ae2bb39f2b4b3f26eb7a4ee595b3
wahaj67 <160678510+wahaj67@users.noreply.github.com>
2024-11-30 22:11:42 +0500

    fixed error

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
-export default function Home(){
+
+import { SessionWrapper } from "@/components/sessionWrapper";
+import Login
+ from "@/components/login/login";
+ import  SignOut  from "@/components/login/signout";
+ import { auth } from "@/lib/auth";
+
+ export default  async function Home(){
+  const session = await auth()
+  const isAuthenticated = !!session?.user
+
   return(
-    <div className="text-3xl font-bold underline ">
-      hello world 
-    </div>
+    <div
+  style={{
+    backgroundImage: "url('/backgrounds.jpg')",
+    backgroundSize: "cover", 
+    backgroundPosition: "center", 
+  }}
+  className="flex relative items-center justify-center h-screen"
+>
+  <h1 className="text-center text-2xl font-sans font-medium">Login</h1>
+  <div className="w-[43%] md:w-[43%] xl:w-[100%] mt-10 flex absolute top-0 items-center justify-items-center max-h-screen rounded-md bg-gradient-to-tr from-pink-300 to-cyan-500">
+    <SessionWrapper>
+      {isAuthenticated ? <SignOut /> : <Login />}
+    </SessionWrapper>
+  </div>
+</div>
+
   )
-}
+ }
