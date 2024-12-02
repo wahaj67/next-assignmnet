59731b6c291310a3d49fb6735a051b358d96dfd2
wahaj67 <160678510+wahaj67@users.noreply.github.com>
2024-12-02 13:26:38 +0500

    al ok

--- components/login/login.tsx
+++ components/login/login.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { FaFacebook } from "react-icons/fa";
+
 import { signIn } from "next-auth/react"
 import { FaGoogle } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import Github from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
-import  Facebook  from "next-auth/providers/facebook"
+
 export const {handlers,signIn,signOut,auth} = NextAuth({
     providers:[
         Github({
