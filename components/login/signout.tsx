fb8780a9457a851018103b838fcad14e3a7951bf
wahaj67 <160678510+wahaj67@users.noreply.github.com>
2024-12-03 02:49:27 +0500

    fixed

--- components/login/signout.tsx
+++ components/login/signout.tsx
@@ -3,7 +3,7 @@ import { signOut } from "next-auth/react";
 export default function SignOut (){
     return(
         <div>
-            <button className="px-4 py-2 mx-auto  bg-black text-white rounded-lg items-center" onClick={()=>signOut()}>
+            <button className=" flex justify-center px-4 py-2 mx-auto  bg-black text-white rounded-lg items-center" onClick={()=>signOut()}>
                   Sign Out
             </button>
         </div>
