40010442d2c2269bf56fea698afee36b6903f459
wahaj67 <160678510+wahaj67@users.noreply.github.com>
2024-12-03 02:41:52 +0500

    com

--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,23 @@ import SignOut
 import {auth} from "@/lib/auth"
 
 export default async function Home() {
+  
   const session = await auth()
   const isAuthennticated = !!session?.user
 
   return(
     <div
-  style={{
-    backgroundImage: "url('/gg.jpg')",
-    backgroundSize: "cover", 
-    backgroundPosition: "center", 
-  }}
+  // style={{
+  //   // background: "linear-gradient(0deg, rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), linear-gradient(0deg, rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), #FFFFFF",
+  //   // backgroundImage: "url('/gg.jpg')",
+  //   // backgroundSize: "cover", 
+  //   // backgroundPosition: "center",
+
+  // }}
   className="flex relative items-center justify-center h-screen"
 >
   
-  <div className="w-[43%]  mt-10 flex absolute top-0 items-center justify-items-center max-h-screen rounded-md bg-gradient-to-tr from-pink-300 to-cyan-500">
+  <div className="w-[43%]  mx-auto mt-10 flex absolute top-0 items-center justify-items-center max-h-screen rounded-md bg-gradient-to-tr from-pink-300 to-cyan-500">
     <SessionWrapper>
       {isAuthennticated ? <SignOut /> : <Login />}
     </SessionWrapper>
